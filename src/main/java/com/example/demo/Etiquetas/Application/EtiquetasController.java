bf85f9c3357eded4720139eb4a9edd345e71677b
BadiRodriguez <badi.rodriguez@utec.edu.pe>
2024-05-21 15:11:33 -0500

    Desperation_1
    Ya estoy asado mano ; - ;

--- src/main/java/com/example/demo/Etiquetas/Application/EtiquetasController.java
+++ src/main/java/com/example/demo/Etiquetas/Application/EtiquetasController.java
@@ -1,10 +1,16 @@
 package com.example.demo.Etiquetas.Application;
 
+import com.example.demo.Etiquetas.Domain.EtiquetasService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping()
 public class EtiquetasController {
+
+    @Autowired
+    private EtiquetasService etiquetasService;
 
 }
