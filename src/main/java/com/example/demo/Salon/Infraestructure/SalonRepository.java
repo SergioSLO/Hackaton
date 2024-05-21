445f4454df5bc2f929b3af139191c805f8190fcd
Sergio Lezama <98142685+SergioSLO@users.noreply.github.com>
2024-05-21 14:31:25 -0500

    Salon Repo

--- src/main/java/com/example/demo/Salon/Infraestructure/SalonRepository.java
+++ src/main/java/com/example/demo/Salon/Infraestructure/SalonRepository.java
@@ -1,10 +1,10 @@
 package com.example.demo.Salon.Infraestructure;
 
-import com.example.demo.Student.Student;
+import com.example.demo.Salon.Domain.Salon;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface SalonRepository extends JpaRepository<Student, Integer> {
+public interface SalonRepository extends JpaRepository<Salon, Integer> {
 
 }
